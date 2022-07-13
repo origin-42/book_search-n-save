0ad2e4a293a47e88b9d89eced94b25d99354d72a
Origin 42 <daveplum1991@gmail.com>
2022-07-13 22:23:39 +0800

    setting up appolloclient, mutations, and queries

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -38,7 +38,7 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(input: Author ): User
+        saveBook(input: Author): User
         removeBook(bookId: String!): User
     }
 `;
